141e13de94a68560fcedfe9b60ffca2e56592e49
Olivier Delree <olivierdelree@protonmail.com>
2024-07-23 17:07:50 +0100

    Histalign/CCF: Factor out data directory discovery and resolution check

--- src/histalign/backend/ccf/allen_downloads.py
+++ src/histalign/backend/ccf/allen_downloads.py
@@ -2,6 +2,7 @@
 #
 # SPDX-License-Identifier: MIT
 
+import os
 from pathlib import Path
 import shutil
 import ssl
@@ -17,29 +18,26 @@ BASE_ATLAS_URL = (
     "https://download.alleninstitute.org/informatics-archive/current-release/mouse_ccf"
 )
 
+data_directories = QtCore.QStandardPaths.standardLocations(
+    QtCore.QStandardPaths.GenericDataLocation
+)
+if not data_directories:
+    raise ValueError("Cannot find a data directory.")
+ATLAS_ROOT_DIRECTORY = Path(data_directories[0]) / "histalign" / "atlases"
+os.makedirs(ATLAS_ROOT_DIRECTORY, exist_ok=True)
+
 
 def get_atlas_path(
     resolution: int,
     atlas_type: Literal["average_template", "ara_nissl"] = "average_template",
 ):
-    if resolution not in ALLOWED_RESOLUTIONS:
-        raise ValueError(
-            f"Invalid resolution. Allowed: {ALLOWED_RESOLUTIONS}, got {resolution}."
-        )
-
-    data_directory = QtCore.QStandardPaths.standardLocations(
-        QtCore.QStandardPaths.StandardLocation.GenericDataLocation
-    )
-    if not data_directory:
-        raise ValueError("Cannot find a data directory.")
-
-    data_directory = data_directory[0]
+    ensure_valid_resolution(resolution)
 
     atlas_file_name = f"{atlas_type}_{resolution}.nrrd"
-    atlas_path = str(Path(data_directory) / atlas_file_name)
+    atlas_path = ATLAS_ROOT_DIRECTORY / atlas_file_name
 
-    if Path(atlas_path).exists():
-        return atlas_path
+    if atlas_path.exists():
+        return str(atlas_path)
     else:
         return download_atlas(resolution, atlas_type)
 
@@ -48,22 +46,11 @@ def download_atlas(
     resolution: int,
     atlas_type: Literal["average_template", "ara_nissl"] = "average_template",
 ) -> str:
-    if resolution not in ALLOWED_RESOLUTIONS:
-        raise ValueError(
-            f"Invalid resolution. Allowed: {ALLOWED_RESOLUTIONS}, got {resolution}."
-        )
-
-    data_directory = QtCore.QStandardPaths.standardLocations(
-        QtCore.QStandardPaths.StandardLocation.GenericDataLocation
-    )
-    if not data_directory:
-        raise ValueError("Cannot find a data directory to download to.")
-
-    data_directory = data_directory[0]
+    ensure_valid_resolution(resolution)
 
     atlas_file_name = f"{atlas_type}_{resolution}.nrrd"
     url = "/".join([BASE_ATLAS_URL, atlas_type, atlas_file_name])
-    atlas_path = str(Path(data_directory) / atlas_file_name)
+    atlas_path = ATLAS_ROOT_DIRECTORY / atlas_file_name
 
     # Allen SSL certificate is apparently not valid...
     context = get_ssl_context(check_hostname=False, check_certificate=False)
@@ -71,7 +58,7 @@ def download_atlas(
     with urlopen(url, context=context) as response, open(atlas_path, "wb") as handle:
         shutil.copyfileobj(response, handle)
 
-    return atlas_path
+    return str(atlas_path)
 
 
 def get_ssl_context(
@@ -84,3 +71,10 @@ def get_ssl_context(
         context.verify_mode = ssl.CERT_NONE
 
     return context
+
+
+def ensure_valid_resolution(resolution) -> None:
+    if resolution not in ALLOWED_RESOLUTIONS:
+        raise ValueError(
+            f"Invalid resolution. Allowed: {ALLOWED_RESOLUTIONS}, got {resolution}."
+        )
