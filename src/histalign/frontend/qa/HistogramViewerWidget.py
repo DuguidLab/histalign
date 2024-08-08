5d7f1c310a2922d789cf5eae42715ecfeddcc6ce
Olivier Delrée <olivierdelree@protonmail.com>
2024-08-08 12:08:07 +0100

    Histalign/QA: Add `HistogramViewerWidget` placeholder

---
+++ src/histalign/frontend/qa/HistogramViewerWidget.py
@@ -0,0 +1,13 @@
+# SPDX-FileCopyrightText: 2024-present Olivier Delrée <olivierdelree@protonmail.com>
+#
+# SPDX-License-Identifier: MIT
+
+
+from typing import Optional
+
+from PySide6 import QtCore, QtGui, QtWidgets
+
+
+class HistogramViewerWidget(QtWidgets.QWidget):
+    def __init__(self, parent: Optional[QtWidgets.QWidget] = None) -> None:
+        super().__init__(parent)
