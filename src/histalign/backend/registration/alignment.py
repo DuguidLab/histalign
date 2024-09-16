756c3b4d77364760674bef2d12fa9bd0d781a026
Olivier Delrée <olivierdelree@protonmail.com>
2024-09-16 13:27:00 +0100

    Histalign/Registration: Add caching capabilities
    Volumes can take a reasonably long time to build, so enable caching by
    default.

--- src/histalign/backend/registration/alignment.py
+++ src/histalign/backend/registration/alignment.py
@@ -2,8 +2,10 @@
 #
 # SPDX-License-Identifier: MIT
 
+import hashlib
 import json
 import logging
+import os
 from pathlib import Path
 
 import numpy as np
@@ -12,6 +14,7 @@ import vedo
 from histalign.backend.ccf.downloads import download_structure_mask
 from histalign.backend.ccf.paths import get_structure_mask_path
 from histalign.backend.io import (
+    DATA_ROOT,
     gather_alignment_paths,
     load_alignment_settings,
     load_image,
@@ -24,14 +27,25 @@ from histalign.backend.models import (
 from histalign.backend.registration import Registrator
 from histalign.backend.workspace import VolumeSlicer
 
+ALIGNMENT_VOLUMES_CACHE_DIRECTORY = DATA_ROOT / "alignment_volumes"
+os.makedirs(ALIGNMENT_VOLUMES_CACHE_DIRECTORY, exist_ok=True)
+
 _module_logger = logging.getLogger(__name__)
 
 
-def build_alignment_volume(alignment_directory: str | Path) -> vedo.Volume:
+def build_alignment_volume(
+    alignment_directory: str | Path, use_cache: bool = True
+) -> vedo.Volume:
     if isinstance(alignment_directory, str):
         alignment_directory = Path(alignment_directory)
 
     targets = gather_alignment_paths(alignment_directory)
+
+    targets_hash = generate_hash_from_targets(targets)
+    cache_path = ALIGNMENT_VOLUMES_CACHE_DIRECTORY / f"{targets_hash}.npz"
+    if cache_path.exists() and use_cache:
+        _module_logger.debug("Found cached volume. Loading from file.")
+        return vedo.Volume(np.load(cache_path)["array"])
 
     with open(targets[0]) as handle:
         temp_settings = AlignmentSettings(**json.load(handle))
@@ -77,6 +91,10 @@ def build_alignment_volume(alignment_directory: str | Path) -> vedo.Volume:
     alignment_volume.modified()
     _module_logger.debug(f"Gathered all {len(plane_meshes)} meshes.")
 
+    if use_cache:
+        _module_logger.debug("Caching volume to file.")
+        os.makedirs(ALIGNMENT_VOLUMES_CACHE_DIRECTORY, exist_ok=True)
+        np.savez_compressed(cache_path, array=alignment_volume.tonumpy())
     return alignment_volume
 
 
@@ -90,3 +108,7 @@ def mask_off_structure(
     mask_volume = load_volume(mask_path)
 
     return vedo.Volume(np.where(mask_volume.tonumpy() > 0, volume.tonumpy(), 0))
+
+
+def generate_hash_from_targets(targets: list[Path]) -> str:
+    return hashlib.md5("".join(map(str, targets)).encode("UTF-8")).hexdigest()
