ab279c39d805bbdb6b758088aeb167b64b41e218
Olivier Delrée <olivierdelree@protonmail.com>
2024-11-06 10:57:16 +0000

    Histalign/CommonWidgets+QA: Move `SliceNamesCombBox` to common widgets

--- src/histalign/frontend/common_widgets.py
+++ src/histalign/frontend/common_widgets.py
@@ -10,8 +10,11 @@ import re
 from typing import Any, Callable, Optional
 
 from PySide6 import QtCore, QtGui, QtWidgets
+from matplotlib.backends.backend_qtagg import FigureCanvasQTAgg
+import matplotlib.pyplot as plt
 
 from histalign.backend.ccf.model_view import StructureModel, StructureNode
+from histalign.backend.workspace import HistologySlice
 from histalign.frontend.dialogs import OpenProjectDialog
 from histalign.frontend.pyside_helpers import FakeQtABC, connect_single_shot_slot
 
@@ -1091,3 +1094,63 @@ class AnimatedHeightWidget(QtWidgets.QWidget):
         self.animation_group.start()
 
         return super().hideEvent(event)
+
+
+class SliceNamesComboBox(QtWidgets.QComboBox):
+    file_picked: QtCore.Signal = QtCore.Signal(str)
+
+    name_to_path_map: dict[str, str]
+
+    def __init__(self, parent: Optional[QtWidgets.QWidget] = None) -> None:
+        super().__init__(parent)
+
+        self.name_to_path_map = {}
+
+        self.currentTextChanged.connect(self.notify_change)
+
+        self.setMinimumWidth(300)
+        # Limits number of visible items to 10
+        self.setStyleSheet("QComboBox { combobox-popup: 0; }")
+
+    def parse_results(self, metadata_path: str) -> None:
+        with open(metadata_path) as handle:
+            paths = json.load(handle).get("slice_paths")
+
+        if paths is None:
+            return
+
+        self.clear()
+
+        metadata_root = Path(metadata_path).parent
+        self.addItem("")
+        for file_path in paths:
+            file_name = Path(file_path).stem
+            self.name_to_path_map[file_name] = file_path
+            self.addItem(file_name)
+
+            if (
+                not Path(file_path).exists()
+                or not (
+                    metadata_root
+                    / f"{HistologySlice.generate_file_name_hash(file_path)}.json"
+                ).exists()
+            ):
+                self.model().item(self.findText(file_name)).setEnabled(False)
+
+    def clear(self) -> None:
+        super().clear()
+        self.name_to_path_map = {"": ""}
+
+    @QtCore.Slot()
+    def notify_change(self, file_name) -> None:
+        return
+        self.file_picked.emit(self.name_to_path_map[file_name])
+
+
+class Canvas(FigureCanvasQTAgg):
+    def __init__(self) -> None:
+        figure = plt.Figure()
+
+        self.axes = figure.add_subplot(111)
+
+        super().__init__(figure)
--- src/histalign/frontend/qa/__init__.py
+++ src/histalign/frontend/qa/__init__.py
@@ -2,76 +2,24 @@
 #
 # SPDX-License-Identifier: MIT
 
-import json
 import logging
 from pathlib import Path
 import re
 from typing import Optional
 
-from PySide6 import QtCore, QtGui, QtWidgets
+from PySide6 import QtCore, QtWidgets
 
 from histalign.backend.workspace import HistologySlice, Workspace
 from histalign.frontend.common_widgets import (
     BasicApplicationWindow,
-    BasicMenuBar,
     ProjectDirectoriesComboBox,
     SelectedStructuresWidget,
+    SliceNamesComboBox,
 )
-from histalign.frontend.dialogs import OpenProjectDialog
 from histalign.frontend.qa.measures import HistogramViewerWidget
 from histalign.frontend.qa.viewer import QAViewerWidget
 
 HASHED_DIRECTORY_NAME_PATTERN = re.compile(r"[0-9a-f]{10}")
-
-
-class SliceNamesComboBox(QtWidgets.QComboBox):
-    file_picked: QtCore.Signal = QtCore.Signal(str)
-
-    name_to_path_map: dict[str, str]
-
-    def __init__(self, parent: Optional[QtWidgets.QWidget] = None) -> None:
-        super().__init__(parent)
-
-        self.name_to_path_map = {}
-
-        self.currentTextChanged.connect(self.notify_change)
-
-        self.setMinimumWidth(300)
-        # Limits number of visible items to 10
-        self.setStyleSheet("QComboBox { combobox-popup: 0; }")
-
-    def parse_results(self, metadata_path: str) -> None:
-        with open(metadata_path) as handle:
-            paths = json.load(handle).get("slice_paths")
-
-        if paths is None:
-            return
-
-        self.clear()
-
-        metadata_root = Path(metadata_path).parent
-        self.addItem("")
-        for file_path in paths:
-            file_name = Path(file_path).stem
-            self.name_to_path_map[file_name] = file_path
-            self.addItem(file_name)
-
-            if (
-                not Path(file_path).exists()
-                or not (
-                    metadata_root
-                    / f"{HistologySlice.generate_file_name_hash(file_path)}.json"
-                ).exists()
-            ):
-                self.model().item(self.findText(file_name)).setEnabled(False)
-
-    def clear(self) -> None:
-        super().clear()
-        self.name_to_path_map = {"": ""}
-
-    @QtCore.Slot()
-    def notify_change(self, file_name) -> None:
-        self.file_picked.emit(self.name_to_path_map[file_name])
 
 
 class QAMainWindow(BasicApplicationWindow):
