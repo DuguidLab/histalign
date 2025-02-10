c316f2274c368a83621484a82f2acc0f3e31538e
Olivier Delrée <olivierdelree@protonmail.com>
2025-02-10 10:22:59 +0000

    Histalign/CommonWidgets: Bring `FlowLayout` typing in line with project

--- src/histalign/frontend/common_widgets.py
+++ src/histalign/frontend/common_widgets.py
@@ -2101,11 +2101,11 @@ class ToggleWidget(QtWidgets.QFrame):
 class FlowLayout(QtWidgets.QLayout):
     """A flow layout that rearranges children in rows if they would overflow.
 
-    Taken directly from the official PySide documentation:
+    Adapted from the official PySide documentation:
     https://doc.qt.io/qtforpython-6/examples/example_widgets_layouts_flowlayout.html
     """
 
-    def __init__(self, parent=None):
+    def __init__(self, parent: Optional[QtWidgets.QWidget] = None):
         super().__init__(parent)
 
         if parent is not None:
@@ -2113,85 +2113,75 @@ class FlowLayout(QtWidgets.QLayout):
 
         self._item_list = []
 
-    def __del__(self):
-        item = self.takeAt(0)
-        while item:
-            item = self.takeAt(0)
-
-    def addItem(self, item):
+    def addItem(self, item: QtWidgets.QLayoutItem) -> None:
         self._item_list.append(item)
 
-    def count(self):
+    def count(self) -> int:
         return len(self._item_list)
 
-    def itemAt(self, index):
-        if 0 <= index < len(self._item_list):
-            return self._item_list[index]
-
-        return None
-
-    def takeAt(self, index):
-        if 0 <= index < len(self._item_list):
-            return self._item_list.pop(index)
-
-        return None
-
-    def expandingDirections(self):
+    def expandingDirections(self) -> QtCore.Qt.Orientation:
         return QtCore.Qt.Orientation(0)
 
-    def hasHeightForWidth(self):
+    def hasHeightForWidth(self) -> bool:
         return True
 
-    def heightForWidth(self, width):
-        height = self._do_layout(QtCore.QRect(0, 0, width, 0), True)
-        return height
+    def heightForWidth(self, width: int) -> int:
+        return self._do_layout(QtCore.QRect(0, 0, width, 0), True)
 
-    def setGeometry(self, rect):
-        super().setGeometry(rect)
-        self._do_layout(rect, False)
+    def itemAt(self, index: int) -> Optional[QtWidgets.QLayoutItem]:
+        try:
+            item = self._item_list[index]
+        except IndexError:
+            item = None
 
-    def sizeHint(self):
-        return self.minimumSize()
+        return item
 
-    def minimumSize(self):
+    def minimumSize(self) -> QtCore.QSize:
         size = QtCore.QSize()
 
         for item in self._item_list:
             size = size.expandedTo(item.minimumSize())
 
         size += QtCore.QSize(
-            2 * self.contentsMargins().top(), 2 * self.contentsMargins().top()
+            self.contentsMargins().top() + self.contentsMargins().bottom(),
+            self.contentsMargins().left() + self.contentsMargins().right(),
         )
+
         return size
 
-    def _do_layout(self, rect, test_only):
+    def setGeometry(self, rect: QtCore.QRect) -> None:
+        super().setGeometry(rect)
+        self._do_layout(rect, False)
+
+    def sizeHint(self) -> QtCore.QSize:
+        return self.minimumSize()
+
+    def takeAt(self, index: int) -> Optional[QtWidgets.QLayoutItem]:
+        try:
+            item = self._item_list.pop(index)
+        except IndexError:
+            item = None
+
+        return item
+
+    def _do_layout(self, rect: QtCore.QRect, dry_run: bool = False) -> int:
         x = rect.x()
         y = rect.y()
         line_height = 0
         spacing = self.spacing()
 
         for item in self._item_list:
-            style = item.widget().style()
-            layout_spacing_x = style.layoutSpacing(
-                QtWidgets.QSizePolicy.Frame,
-                QtWidgets.QSizePolicy.Frame,
-                QtCore.Qt.Orientation.Horizontal,
-            )
-            layout_spacing_y = style.layoutSpacing(
-                QtWidgets.QSizePolicy.Frame,
-                QtWidgets.QSizePolicy.Frame,
-                QtCore.Qt.Vertical,
-            )
-            space_x = spacing + layout_spacing_x
-            space_y = spacing + layout_spacing_y
-            next_x = x + item.sizeHint().width() + space_x
-            if next_x - space_x > rect.right() and line_height > 0:
+            # Compute left edge of potential next item
+            next_x = x + item.sizeHint().width() + spacing
+            # Check right edge of current item can fit on current line
+            if next_x - spacing > rect.right():
+                # Go to next line
                 x = rect.x()
-                y = y + line_height + space_y
-                next_x = x + item.sizeHint().width() + space_x
+                y = y + line_height + spacing
+                next_x = x + item.sizeHint().width() + spacing
                 line_height = 0
 
-            if not test_only:
+            if not dry_run:
                 item.setGeometry(QtCore.QRect(QtCore.QPoint(x, y), item.sizeHint()))
 
             x = next_x
