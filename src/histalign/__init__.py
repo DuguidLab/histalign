ca40ac8e1eac09bb2fd65b632846893ae36e74c7
Olivier Delrée <olivierdelree@protonmail.com>
2025-04-17 11:33:21 +0100

    Histalign: Fix logging when running as a script and limit vedo patching
    When running as main, we no longer create an additional logger, ensuring
    the `_module_logger` inside `histalign/__init__.py` is always set for
    the `histalign` package and not using `__main__`.
    
    Additionally, we only patch `vedo` when running as the `histalign`
    module.

--- src/histalign/__init__.py
+++ src/histalign/__init__.py
@@ -2,13 +2,12 @@
 #
 # SPDX-License-Identifier: MIT
 
-if __name__ in ["__main__", "histalign"]:
+if __name__ == "histalign":
     # Fix a typing bug when using `vedo` with python==3.10.12.
     # Leaving `typing_extensions.Self` as-is leads to the following error message:
     # TypeError: Plain typing.Self is not valid as type argument
     # and happens because vedo uses the `Self` annotation in the return type of some
     # methods, while it apparently is malformed.
-    # Avoid being invasive by only patching it when we're the main application.
     from typing import TypeVar
 
     import typing_extensions
@@ -34,18 +33,18 @@ from histalign.io.transform import transform
 PREFERRED_STARTUP_SIZE = QtCore.QSize(1600, 900)
 
 # Set up package logging
-_module_logger = logging.getLogger(__name__)
+_module_logger = logging.getLogger("histalign")
+if __name__ == "histalign":
+    _formatter = logging.Formatter(
+        "[{asctime}] - [{levelname:>8s} ] - {message} ({name}:{lineno})",
+        style="{",
+        datefmt="%Y-%m-%d %H:%M:%S",
+    )
 
-_formatter = logging.Formatter(
-    "[{asctime}] - [{levelname:>8s} ] - {message} ({name}:{lineno})",
-    style="{",
-    datefmt="%Y-%m-%d %H:%M:%S",
-)
+    _console_handler = logging.StreamHandler()
+    _console_handler.setFormatter(_formatter)
 
-_console_handler = logging.StreamHandler()
-_console_handler.setFormatter(_formatter)
-
-_module_logger.addHandler(_console_handler)
+    _module_logger.addHandler(_console_handler)
 
 
 @click.group(invoke_without_command=True)
